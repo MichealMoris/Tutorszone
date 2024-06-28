364bc936b6e326dc4835f9a3037cb1a06dc6575e
MichealMoris <michealmoriswaheeb@gmail.com>
2024-06-29 00:50:23 +0300

    vpn11

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 //     return redirect('/');
 // });
 Route::get('/en/client-ip', function (Request $request) {
-    return response()->json($request->ips());
+    return response()->json($request->ip());
 });
 Route::get('/en/create-symlink', function (){
     symlink(storage_path('/app/public'), public_path('storage'));
